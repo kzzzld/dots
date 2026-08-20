cc02cd1240f892ff176405f1652d1b9fe72ab2b1
kzzzld <kzzzl@proton.me>
2026-08-20 12:05:30 +0300

    feat(vscodium): remove all extension things because homebrew already can do it

--- install.sh
+++ install.sh
@@ -102,12 +102,6 @@ sudo defaults write /Library/Managed\ Preferences/com.brave.Browser BraveP3AEnab
 sudo defaults write /Library/Managed\ Preferences/com.brave.Browser BraveStatsPingEnabled -bool false
 sudo defaults write /Library/Managed\ Preferences/com.brave.Browser BraveWebDiscoveryEnabled -bool false
 
-# VSCodium
-defaults write com.vscodium ApplePressAndHoldEnabled -bool false
-for i in ${VSCODIUM_EXTENSION_LIST[@]}; do
-  codium --install-extension $i
-done
-
 # Ubersicht
 [ ! -d "$HOME/Library/Application Support/Übersicht/widgets/simple-bar" ] && git clone --depth 1 https://github.com/Jean-Tinland/simple-bar $HOME/Library/Application\ Support/Übersicht/widgets/simple-bar
 
