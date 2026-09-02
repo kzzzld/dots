fae855121b84688d564e295680df440265ce336e
kzzzld <kzzzl@kzzzl.xyz>
2026-09-02 21:13:52 +0300

    feat: move positions of screens

--- linux/.config/hypr/hyprland.lua
+++ linux/.config/hypr/hyprland.lua
@@ -4,9 +4,16 @@
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
+	output = "DP-1",
+	mode = "preferred",
+	position = "0x0",
+	scale = "1",
+})
+
+hl.monitor({
 	output = "eDP-1",
 	mode = "preferred",
-	position = "auto",
+	position = "0x1440",
 	scale = "1.6",
 })
 
@@ -332,11 +339,17 @@ hl.window_rule({
 	float = true,
 })
 
-hl.workspace_rule({ workspace = "1", monitor = "eDP-1", persistent = true })
-hl.workspace_rule({ workspace = "2", monitor = "eDP-1", persistent = true })
-hl.workspace_rule({ workspace = "3", monitor = "eDP-1", persistent = true })
-hl.workspace_rule({ workspace = "4", monitor = "eDP-1", persistent = true })
-hl.workspace_rule({ workspace = "5", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "1", monitor = "DP-1", persistent = true })
+hl.workspace_rule({ workspace = "2", monitor = "DP-1", persistent = true })
+hl.workspace_rule({ workspace = "3", monitor = "DP-1", persistent = true })
+hl.workspace_rule({ workspace = "4", monitor = "DP-1", persistent = true })
+hl.workspace_rule({ workspace = "5", monitor = "DP-1", persistent = true })
+hl.workspace_rule({ workspace = "5", monitor = "DP-1", persistent = true })
+hl.workspace_rule({ workspace = "6", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "7", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "8", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "9", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "10", monitor = "eDP-1", persistent = true })
 
 -- For Noctalia Color templates
 require("noctalia").apply_theme()
